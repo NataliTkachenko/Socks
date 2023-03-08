6e2cd2cfd1c9e9ef103e75da1403c44318351da8
Isa <isa_akashev@mail.ru>
2023-03-08 19:01:57 +0300

    Sign

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import Cart from '../components/Cart';
-import Navbar from '../components/Navbar';
-import Favourites from '../components/Favourites';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './Cart';
+import Navbar from './Navbar';
+import Favourites from './Favourites';
+import SignInPage from './Auth/SingInPage';
+import SignUpPage from './Auth/SingUpPage';
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Routes>
-        <Route path="/favourites" element={<Favourites  />} />
-        <Route path="/cart" element={<Cart  />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/user/signup" element={<SignUpPage />} />
         <Route path="/user/signin" element={<SignInPage />} />
       </Routes>
 
-      
     </div>
-  )
+  );
 }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import jsxRender from './utils/jsxRender';
+import authRouters from './routes/authRouters';
 
 const PORT = 3000;
 const app = express();
@@ -20,19 +21,18 @@ app.get('/', (req, res) => {
   res.render('Layout', initState);
 });
 
+app.use('/user/', authRouters);
+
 app.listen(PORT, () => console.log(`App has started on port ${PORT}`));
 
-
-app.get('/cart', (req,res)=>{
-  res.render('Layout',{})
+app.get('/cart', (req, res) => {
+  res.render('Layout', {});
 });
 
-
-app.get('/favourites', (req,res)=>{
-  res.render('Layout',{})
+app.get('/favourites', (req, res) => {
+  res.render('Layout', {});
 });
 
-
-app.get('/cart', (req,res)=>{
-  res.render('Layout',{})
+app.get('/cart', (req, res) => {
+  res.render('Layout', {});
 });

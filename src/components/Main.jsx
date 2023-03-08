9a44bba31c0af7d35a50ed5703ddce1f70e55d06
Nataly <nataly@MacBook-Air-2.local>
2023-03-08 19:03:28 +0300

    release3

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function Main() {
+  const styles = {
+    backgroundColor: 'grey',
+    padding: '20px'
+  };
+
+  const btnStyles = {
+    backgroundColor: 'turquoise',
+    color: 'white',
+    padding: '10px',
+    borderRadius: '5px',
+    border: 'none',
+    cursor: 'pointer'
+  };
+
   return (
-    <div>Main</div>
-  )
+    <div style={styles}>
+      <h1>Самое время быть уникальным!</h1>
+      <button style={btnStyles} type="submit">Смоделируй свою любимую пару носков!</button>
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,33 @@ import axios from 'axios';
 import React from 'react';
 
 export default function Navbar() {
-  
+  const btnStyles = {
+    backgroundColor: 'turquoise',
+    color: 'white',
+    padding: '10px',
+    borderRadius: '5px',
+    border: 'none',
+    cursor: 'pointer'
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-beige">
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/favourites">Избранное</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/cart">Корзина</a>
             </li>
+          </ul>
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="/user/signup">Зарегистрироваться</a>
+              <button className="nav-link btn" style={btnStyles} href="/user/signup">Зарегистрироваться</button>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/user/signin">Войти</a>
+              <button className="nav-link btn" style={btnStyles} href="/user/signin">Войти</button>
             </li>
           </ul>
         </div>
